b1c0b291893676c1b0dbe5cc6efb43de210422b7
bouzenaali <Alibouzena16@gmail.com>
2023-04-10 12:41:17 +0100

    fix: closed the input after the program finishes

--- src/DateValidation.java
+++ src/DateValidation.java
@@ -5,15 +5,16 @@ import java.util.Scanner;
 
 public class DateValidation {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Entrez une date au format JJ/MM/AAAA : ");
-        String input = scanner.nextLine();
-        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        try {
-            LocalDate date = LocalDate.parse(input, dateFormatter);
-            System.out.println("La date entrée est : " + date);
-        } catch (DateTimeException e) {
-            System.out.println("Erreur : format de date invalide !");
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Entrez une date au format JJ/MM/AAAA : ");
+            String input = scanner.nextLine();
+            DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            try {
+                LocalDate date = LocalDate.parse(input, dateFormatter);
+                System.out.println("La date entrée est : " + date);
+            } catch (DateTimeException e) {
+                System.out.println("Erreur : format de date invalide !");
+            }
         }
     }
 }
